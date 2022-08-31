4020aab3b613a70d2811f03a2035b00942918c0e
Paulo Lacerda <prlacerda28@gmail.com>
2022-08-30 22:14:50 -0300

    groups

---
+++ app/Controllers/Http/GroupsController.ts
@@ -0,0 +1,11 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Group from 'App/Models/Group'
+import CreateGroup from 'App/Validators/CreateGroupValidator'
+
+export default class GroupsController {
+  public async store({ request, response }: HttpContextContract) {
+    const groupPayload = await request.validate(CreateGroup)
+    const group = await Group.create(groupPayload)
+    return response.created({ group })
+  }
+}
